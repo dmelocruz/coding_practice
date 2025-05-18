7a87c6525b0da23874e0a5b2fb88a8fd34f57cbd
Daniel Cruz <dan30cruz@gmail.com>
2025-05-17 22:03:09 -0300

    Deleted old files

--- big_o/o_n2_ex2.java
+++
@@ -1,9 +0,0 @@
-// Time Complexity: O(n^2)
-
-void printPairs(int[] array){
-    for(int i = 0; i < array.length; i++){
-        for(int j = 0; j < array.length; j++){
-            System.out.prinln(array[i] + "," + array[j]);
-        }
-    }
-}
