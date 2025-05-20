29b04910cb23538c661e4b7156244419beea1c9c
Daniel Cruz <dan30cruz@gmail.com>
2025-05-19 22:26:08 -0300

    example 11

---
+++ big_o/big_o_ex11.java
@@ -0,0 +1,13 @@
+// Time complexity: O(n)
+
+int factorial(int n){
+    if(n < 0){
+        return -1;
+    }
+    else if(n == 0){
+        return 1;
+    }
+    else{
+        return n * factorial(n - 1);
+    }
+}
